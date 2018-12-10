e84784c5ad1cd8d56e056a4f9d61fc153beeab89
Travis Burrows <37048572+tjburrows@users.noreply.github.com>
2018-12-10 12:26:00 -0500

    Upload initial version

---
+++ BatchVecterConvert.py
@@ -0,0 +1,57 @@
+"""
+Batch Vector File Conversion with Inkscape
+Travis Burrows 12/10/2018
+
+This script batch converts all files of a specified extension (extIn) into another file format (extOut)
+in the specified directory dirConvert.   This requires that inkscape is installed, and the path to the
+executable is specified in inkscapePath.  All supported output formats have been tested but not all
+input formats.  In addition, there are many more formats that are supported that are not currently
+explicitly listed as supported.  This script has only been tested on Windows, but it should work on other
+OSes, except for support for the wmf file extension.
+
+To use, Change extIn to the format desired for input.  Change extOut to the desired format for output.
+Change dirConvert to the directory of the files desired to convert.  Change inkscapePath to the path
+of your inkscape executable. 
+"""
+
+import subprocess
+import os
+
+#   Input file extension
+extIn = 'wmf'
+
+#   Output file extension
+extOut = 'svg'
+
+#   File directory
+dirConvert = r'C:\Users\labuser\Nextcloud\ME 7751\Project\test'
+
+#   Inkscape executable path
+inkscapePath = r'C:\Program Files\Inkscape\inkscape.exe'
+
+#   Supported formats for input (not nearly exhaustive)
+supportedIn = ['svg','wmf','emf','ps','pdf','eps']
+
+#   Supported formats for output
+supportedOut = ['svg','wmf','emf','ps','pdf','eps','png']
+
+#   Test for valid extensions
+if (extIn not in supportedIn):
+    raise ValueError("Requested input extension is not supported.  It must be one of the following:\n%s" % supportedIn)
+if (extOut not in supportedOut):
+    raise ValueError("Requested output extension is not supported.  It must be one of the following:\n%s" % supportedOut)
+
+if extOut == 'svg':
+    extOutArgument = '--export-plain-svg='
+else:
+    extOutArgument = '--export-%s=' % extOut
+
+#   Loop over files in directory and convert
+files=next(os.walk(dirConvert))[2]
+lenext = len(extIn)
+for file in files:
+    inpath = os.path.join(dirConvert,file)
+    if inpath.endswith(extIn):
+        outpath = inpath[:-lenext] + extOut
+        commandstring = '"' + inkscapePath +   '" --file="' + inpath + '" ' + extOutArgument + '"' + outpath + '"'
+        subprocess.run(commandstring)
